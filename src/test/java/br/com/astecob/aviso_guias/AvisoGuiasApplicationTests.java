5de9a1cb88d8e773971ec127b277738abb2f1342
vinig <vinigamexd12@gmail.com>
2026-09-17 12:34:32 -0300

    add mockitoBean in AvisoGuiasApplicationTest

--- src/test/java/br/com/astecob/aviso_guias/AvisoGuiasApplicationTests.java
+++ src/test/java/br/com/astecob/aviso_guias/AvisoGuiasApplicationTests.java
@@ -1,12 +1,17 @@
 package br.com.astecob.aviso_guias;
 
+import br.com.astecob.aviso_guias.application.port.EmailSender;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class AvisoGuiasApplicationTests {
+
+	@MockitoBean
+	private EmailSender emailSender;
 
 	@Test
 	void contextLoads() {
